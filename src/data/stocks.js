c0479d93c8998e09ac7e6a9b017a195997862fdd
Alisson Comerlatto <comerlattoalisson@gmail.com>
2020-09-12 22:41:24 -0300

    feat: add dummy data for stocks price

---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id : 1, name : 'Google', price : 200 },
+  { id : 2, name : 'Twitter', price : 139 },
+  { id : 3, name : 'Facebook', price : 22 },
+  { id : 4, name : 'Microsft', price : 145 },
+];
