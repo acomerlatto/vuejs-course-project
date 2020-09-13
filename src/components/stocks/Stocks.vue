ea0a706461480b1d0ddaa7ac7db742fa4c773bea
Alisson Comerlatto <comerlattoalisson@gmail.com>
2020-09-12 22:25:11 -0300

    feat(components): add new elements to stock and stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,27 @@
 <template>
-    
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
+  import Stock from "./Stock.vue";
+
   export default {
-    name : "Stocks"
+    name : "Stocks",
+    components : {
+      appStock : Stock
+    },
+    data() {
+      return {
+        stocks : [
+          { id : 1, name : 'Google', price : 200 },
+          { id : 2, name : 'Twitter', price : 200 },
+          { id : 3, name : 'Facebook', price : 200 },
+          { id : 4, name : 'Microsft', price : 200 },
+        ]
+      };
+    }
   };
 </script>
 
